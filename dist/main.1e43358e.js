917e4da5948bfc65022dc18f2ca8ba0cf3c2d6de
Laurence Taylor <ltaylorapp@gmail.com>
2019-09-12 17:34:46 +0100

    Added Cyprus

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -30705,7 +30705,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52479" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54227" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
